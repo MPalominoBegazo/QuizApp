56644cc8b446b974d27599f1bfe5a999a22ac4ac
Maria del Rosario Palomino Begazo <100kj.mr@gmail.com>
2017-09-18 11:13:16 -0700

    use

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+"use strict";
 /*Menu BUTTONS */
 $(document).ready(function () {
     $("#categoriesDiv").click(function () {
--- js/order3.js
+++ js/order3.js
@@ -1,3 +1,4 @@
+"use strict";
 let appQuiz = {
     items: {
         quesGeography: undefined,
